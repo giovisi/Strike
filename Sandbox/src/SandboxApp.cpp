978d272984b1ab8c68cadec61acc45efac760150
Gio <giovannivisi@gmail.com>
2024-12-02 18:22:34 +0100

    Added Transforms.

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -2,10 +2,12 @@
 
 #include "imgui/imgui.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 class ExampleLayer : public Strike::Layer {
 public:
 	ExampleLayer()
-		: Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f) {
+		: Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f), m_SquarePosition(0.0f) {
 		m_VertexArray.reset(Strike::VertexArray::Create());
 
 		float vertices[3 * 7] = {
@@ -33,10 +35,10 @@ public:
 		m_SquareVA.reset(Strike::VertexArray::Create());
 
 		float squareVertices[3 * 4] = {
-			-0.75f, -0.75f,  0.0f,
-			 0.75f, -0.75f,  0.0f,
-			 0.75f,  0.75f,  0.0f,
-			-0.75f,  0.75f,  0.0f
+			-0.5f, -0.5f,  0.0f,
+			 0.5f, -0.5f,  0.0f,
+			 0.5f,  0.5f,  0.0f,
+			-0.5f,  0.5f,  0.0f
 		};
 		std::shared_ptr<Strike::VertexBuffer> squareVB;
 		squareVB.reset(Strike::VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
@@ -59,6 +61,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -66,7 +69,7 @@ public:
 			void main() {
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -92,13 +95,14 @@ public:
 			layout(location = 0) in vec3 a_Position;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
 
 			void main() {
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -118,7 +122,7 @@ public:
 	}
 
 	void OnUpdate(Strike::Timestep ts) override {
-		STRK_TRACE("Delta time : {0}s ({1}ms)", ts.GetSeconds(), ts.GetMilliseconds());
+		//STRK_TRACE("Delta time : {0}s ({1}ms)", ts.GetSeconds(), ts.GetMilliseconds());
 
 		if (Strike::Input::IsKeyPressed(STRK_KEY_LEFT))
 			m_CameraPosition.x -= m_CameraMoveSpeed * ts;
@@ -144,7 +148,16 @@ public:
 
 		Strike::Renderer::BeginScene(m_Camera);
 
-		Strike::Renderer::Submit(m_BlueShader, m_SquareVA);
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		for (int y = 0; y < 20; y++) {
+			for (int x = 0; x < 20; x++) {
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				Strike::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+			}
+		}
+
 		Strike::Renderer::Submit(m_Shader, m_VertexArray);
 
 		Strike::Renderer::EndScene();
