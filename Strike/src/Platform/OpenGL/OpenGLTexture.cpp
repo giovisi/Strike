9e5bb058b32264e0eb0e4eb83758ed12be7e1425
Gio <giovannivisi@gmail.com>
2024-12-03 13:55:55 +0100

    Modified Textures.

--- Strike/src/Platform/OpenGL/OpenGLTexture.cpp
+++ Strike/src/Platform/OpenGL/OpenGLTexture.cpp
@@ -15,6 +15,8 @@ namespace Strike {
 		m_Width = width;
 		m_Height = height;
 
+		// Engine series version (not working)
+
 		//glCreateTextures(GL_TEXTURE_2D, 1, &m_RendererID);
 		//glTextureStorage2D(m_RendererID, 1, GL_RGB8, m_Width, m_Height);
 
@@ -23,18 +25,21 @@ namespace Strike {
 
 		//glTextureSubImage2D(m_RendererID, 0, 0, 0, m_Width, m_Height, GL_RGB, GL_UNSIGNED_BYTE, data);
 
-		//glGenTextures(1, &m_RendererID);
-		//glBindTexture(GL_TEXTURE_2D, m_RendererID);
 
+		// OpenGL series version (working)
+
+		glGenTextures(1, &m_RendererID);
+		glBindTexture(GL_TEXTURE_2D, m_RendererID);
+		
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
+		
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-
+		
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_Width, m_Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
 		glBindTexture(GL_TEXTURE_2D, 0);
-
+		
 		stbi_image_free(data);
 	}
 
